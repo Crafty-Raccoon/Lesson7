42d1a2b8b3173776203f2a906243a46d4eaaaef8
DESKTOP-QP6E4JG\Valtas <Kostoev-23@yandex.ru>
2022-10-28 13:10:44 +0300

    task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//m = 3, n = 4.
+
+//0,5 7 -2 -0,2
+
+//1 -3,3 8 -9,9
+
+//8 7,8 -7,1 9
+
+double GenerateDouble(double min, double max)
+{
+    Random rnd = new Random();
+    double number = rnd.NextDouble();
+    double rndDouble = number * (max - min) + min;
+    return rndDouble;
+}
+
+void Fill2dArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = GenerateDouble(-10, 10);
+        }
+    }
+}
+
+void Print2dArray(double[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(arr[i, j], 2)} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк массива");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int n = int.Parse(Console.ReadLine());
+double[,] array = new double[m, n];
+
+Fill2dArray(array);
+Print2dArray(array);
